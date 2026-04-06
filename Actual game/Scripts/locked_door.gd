9a3729e59e934b9e9e2f2cfdd62c61f67fc63578
NICHOLASALEPALE <126365636+NICHOLASALEPALE@users.noreply.github.com>
2026-04-05 21:40:20 -0700

    more damn rooms

---
+++ Actual game/Scripts/locked_door.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+var player_in_range: Dwight = null
+@onready var room = get_parent()
+
+#var bg_freed = preload("res://path_to/birdroom3i_bg2.png")
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Dwight:
+		player_in_range = body
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is Dwight:
+		player_in_range = null
+
+func _process(delta: float) -> void:
+	if player_in_range != null and Input.is_action_just_pressed("ui_select"):
+		if not GameManager.has_key:
+			player_in_range.show_interact_text("It's locked.")
+			await get_tree().create_timer(1).timeout
+			player_in_range.show_interact_text(" ")
+		else:
+			pass
+			#background.texture = bg_freed
