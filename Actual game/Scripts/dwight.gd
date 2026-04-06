025cb162c3e6058a67410fea727ff47fd5b6744d
NICHOLASALEPALE <126365636+NICHOLASALEPALE@users.noreply.github.com>
2026-04-05 20:25:51 -0700

    new rooms

--- Actual game/Scripts/dwight.gd
+++ Actual game/Scripts/dwight.gd
@@ -14,7 +14,9 @@ var current_hide_message: String = ""
 var bread_key_message: String = ""
 var chosen: bool = false
 
-@onready var sprite: Sprite2D = $Sprite2D
+@onready var amimated_sprite_2d: AnimatedSprite2D = $AmimatedSprite2D
+var facing_right: bool = true
+
 @onready var collision: CollisionShape2D = $CollisionShape2D
 @onready var hide_text_label: Label = $"../HideTextLabel"
 @onready var hide_text_timer: Timer = $"../HideTextTimer"
@@ -45,15 +47,23 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Uncomment if you want jumping
-	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	# 	velocity.y = JUMP_VELOCITY
-
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction != 0:
+
+	if direction > 0:
 		velocity.x = direction * SPEED
+		facing_right = true
+		amimated_sprite_2d.play("walk_right")
+	elif direction < 0:
+		velocity.x = direction * SPEED
+		facing_right = false
+		amimated_sprite_2d.play("walk_left")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+		if facing_right:
+			amimated_sprite_2d.play("idle_right")
+		else:
+			amimated_sprite_2d.play("idle_left")
 
 	move_and_slide()
 
@@ -62,13 +72,13 @@ func toggle_hide() -> void:
 
 	if is_hiding:
 		hide_position = global_position
-		sprite.visible = false
+		amimated_sprite_2d.visible = false
 		collision.disabled = true
 		show_hide_text(current_hide_message)
 
 	else:
 		global_position = hide_position
-		sprite.visible = true
+		amimated_sprite_2d.visible = true
 		collision.disabled = false
 
 func has_chosen() -> void:
